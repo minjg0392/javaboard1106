77fdb8c7389dfacaf31dd89df4af6dfd610a4a42
minjg0392 <abb0392@naver.com>
2021-11-06 11:46:48 +0900

    pagingtestmodify

--- board/PagingTest.java
+++ board/PagingTest.java
@@ -6,11 +6,37 @@ public class PagingTest {
 	
 	public static void main(String[] args) {
 		
+		
 		Scanner sc = new Scanner(System.in);
-		
+			
 		int currentPageNo = 1;
-		
+			
 		while(true) {
+			// 1 / 5 - 1
+			// 2 / 5 - 1
+			// 3 / 5 - 1
+			// 4 / 5 - 1
+			// 5 / 5 - 1
+
+			// 6 / 5 - 2
+			// 7 / 5 - 2
+			// 10 / 5 - 2
+
+			// 올림(현재페이지 / 한블럭당페이지개수)
+			int currentBlockNo = (int)Math.ceil((double)currentPageNo / 5);
+
+			int startPageNoInBlock = 5 * (currentBlockNo - 1) + 1;  
+			int endPageNoInBlock = startPageNoInBlock + 4;
+
+			// 2. 현재페이지가 5 > 6으로 바뀔 때 페이지를 6 7 8 9 10
+			for(int i = startPageNoInBlock; i <= endPageNoInBlock; i++) {
+				if(i == currentPageNo) {
+					System.out.print("[" + i + "] ");
+				} else {
+					System.out.print(i + " ");
+				}
+			}
+			System.out.println();
 			System.out.print("페이징 명령어를 입력해주세요 ((prev : 이전,  next : 다음,  go : 선택,  back : 뒤로가기):");
 			String pageCmd = sc.nextLine();
 
@@ -21,34 +47,18 @@ public class PagingTest {
 			} else if(pageCmd.equals("go")) {
 				System.out.print("몇번 페이지로 이동하시겠습니까 : ");
 				currentPageNo = Integer.parseInt(sc.nextLine());
+			}  else if(pageCmd.equals("back")) {
+				
 			}
 
-			// 2. 현재페이지가 5 > 6으로 바뀔 때 페이지를 6 7 8 9 10
-			
-			
-			if(currentPageNo <= 5) {
-				for(int i = 1; i <= 5; i++) {
-					if(i == currentPageNo) {
-						System.out.print("[" + i + "] ");				
-					} else {
-						System.out.print(i + " ");
-					}
-				}
-			} else if(currentPageNo > 5) {
-				for(int i = 6; i <= 10; i++) {
-					if(i == currentPageNo) {
-						System.out.print("[" + i + "] ");				
-					} else {
-						System.out.print(i + " ");
-					}
-				}
-			}
+
 			
 		}
-		
-		
+
 		
 		
 	}
+		
+		
 
 }
